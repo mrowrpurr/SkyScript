4fc93502fa78f51befe269599c6a9c3fef8e8066
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-07 22:51:52 -0800

    YAML is parsing thanks to yaml-cpp

--- include/SkyScript/YamlReader.h
+++ include/SkyScript/YamlReader.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <ryml/ryml_std.hpp>
-#include <ryml/ryml.hpp>
+#include <yaml-cpp/yaml.h>
 
 #include "Context.h"
 #include "IDocumentReader.h"
@@ -13,13 +12,16 @@ namespace SkyScript {
 	 */
 	class YamlReader : public IDocumentReader {
 	private:
-		c4::yml::Tree _tree;
-		c4::yml::NodeRef _currentLocation;
+		YAML::Node _root;
 
 	public:
 		explicit YamlReader(std::string yamlText) {
-			_tree = ryml::parse(ryml::to_substr((yamlText)));
-			_currentLocation = _tree;
+			_root = YAML::Load(yamlText);
+			std::cout << std::format("YAML TEXT: '{}'", yamlText);
+			std::cout << std::format("IS MAP? {}", _root.IsMap());
+			for(YAML::const_iterator node = _root.begin(); node != _root.end(); ++node) {
+				std::cout << std::format("LINE: {} KEY: {} VALUE: {}", node->first.Mark().line, node->first.as<std::string>(), node->second.as<std::string>());
+			}
 		}
 
 		bool IsMap() override {
@@ -31,7 +33,7 @@ namespace SkyScript {
 		}
 
 		std::string GetFunctionName() override {
-			return "";
+			return "TESTING!";
 		}
 	};
 }
--- test/integration/ParsingClassDefinitionsTest.h
+++ test/integration/ParsingClassDefinitionsTest.h
@@ -2,13 +2,17 @@
 
 #include "TestHelper.h"
 
+#include <SkyScript/ScriptFile.h>
+
 class ParsingClassDefinitionsTest : public SkyScriptIntegrationTest {};
 
 TEST_F(ParsingClassDefinitionsTest, ParseClassWithName) {
-	auto context = Eval(R"(
-class:
-	:name: hello
-)");
+//	auto context = Eval(R"(
+//hello: world
+//foo: bar
+//)");
+	auto content = ScriptFile::GetFileText("C:/Code/test.yaml");
+	auto context = Eval(content);
 
 	ASSERT_EQ(context.GetTypeCount(), 1);
 	ASSERT_TRUE(context.TypeExists("Dog"));
