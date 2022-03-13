96531ee89009d7494f951688a6c1edc8342b551d
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-13 09:09:55 -0700

    Add helper .bat for running parser specs

---
+++ run_parser_specs.bat
@@ -0,0 +1,10 @@
+echo off
+
+set argCount=0
+for %%x in (%*) do set /A argCount+=1
+
+if "%argCount%" == "1" (
+    .\build\Debug\spec\parser\Debug\spec-parser.exe --only="%1%"
+) else (
+    .\build\Debug\spec\parser\Debug\spec-parser.exe %*
+)
