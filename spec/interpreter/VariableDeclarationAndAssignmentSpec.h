074f83866c8e3b11ec57d1c1f6117bc52500f316
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-13 19:59:17 -0700

    TODO specs for variables incl implicit typing

--- spec/interpreter/VariableDeclarationAndAssignmentSpec.h
+++ spec/interpreter/VariableDeclarationAndAssignmentSpec.h
@@ -4,14 +4,23 @@
 
 go_bandit([](){
     describe("Variable Declaration and Assignment", [](){
-        xit("can declare a primitive __string__ variable", [&](){
-//            auto context = ContextImpl();
-//            AssertThat(context.FunctionCount(), Equals(0));
-//            AssertThat(context.FunctionExists("hello"), IsFalse());
-//
-//            Eval(context, R"(
-//hello():
-//)");
+        xit("can declare a primitive __string__ variable without a value", [&](){
+            auto context = ContextImpl();
+            AssertThat(context.VariableCount(), Equals(0));
+            AssertThat(context.VariableExists("foo"), IsFalse());
+
+            Eval(context, R"(
+- AnyType foo:
+)");
+
+            AssertThat(context.VariableCount(), Equals(1));
+            AssertThat(context.VariableExists("foo"), IsTrue());
+//            AssertThat(context.GetVariable("foo").GetTypeName(), Equals("string"));
         });
+        xit("can declare and assign a primitive __string__ variable with a value", [&](){});
+        xit("has implicit typing for integers", [&](){});
+        xit("has implicit typing for floats", [&](){});
+        xit("has implicit typing for booleans", [&](){});
+        xit("has implicit typing for strings", [&](){});
     });
 });
