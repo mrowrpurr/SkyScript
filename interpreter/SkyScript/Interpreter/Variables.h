41256946cbd9b2942f462996ffa9d979dc0d6f1f
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-16 20:37:05 -0700

    Added implicit typing for primitive variables

--- interpreter/SkyScript/Interpreter/Variables.h
+++ interpreter/SkyScript/Interpreter/Variables.h
@@ -10,7 +10,7 @@ namespace SkyScript::Interpreter::Variables {
     }
 
     void AddOrUpdateVariableInContext(SkyScriptNode& node, ContextImpl& context) {
-        std::string variableType = "stdlib::string";
+        std::string variableType;
         std::string variableName = node.GetSingleKey();
         variableName = variableName.substr(0, variableName.length() - 2); // 2 for " ="
 
@@ -22,11 +22,26 @@ namespace SkyScript::Interpreter::Variables {
         }
 
         auto& nodeValue = node.GetSingleValue();
+
+        if (variableType.empty()) {
+            if (nodeValue.IsString()) {
+                variableType = "stdlib::string";
+            } else if (nodeValue.IsInteger()) {
+                variableType = "stdlib::int";
+            } else if (nodeValue.IsFloat()) {
+                variableType = "stdlib::float";
+            } else if (nodeValue.IsBool()) {
+                variableType = "stdlib::bool";
+            }
+        }
+
         auto variableValue = TypedValueImpl(variableType);
         if (variableType == "int" || variableType == "stdlib::int") {
             variableValue.SetValue(nodeValue.GetIntValue());
         } else if (variableType == "float" || variableType == "stdlib::float") {
                 variableValue.SetValue(nodeValue.GetFloatValue());
+        } else if (variableType == "bool" || variableType == "stdlib::bool") {
+            variableValue.SetValue(nodeValue.GetBoolValue());
         } else {
             variableValue.SetValue(nodeValue.GetStringValue());
         }
--- spec/interpreter/VariablesSpec.h
+++ spec/interpreter/VariablesSpec.h
@@ -64,9 +64,127 @@ go_bandit([](){
             AssertThat(value.GetValue<double>(), Equals(69.420));
             AssertThat(value.GetFloatValue(), Equals(69.420));
         });
-        xit("has implicit typing for strings", [&](){});
-        xit("has implicit typing for integers", [&](){});
-        xit("has implicit typing for floats", [&](){});
-        xit("has implicit typing for booleans", [&](){});
+        it("can declare an bool variable", [&](){
+            auto context = ContextImpl();
+            AssertThat(context.VariableCount(), Equals(0));
+            AssertThat(context.VariableExists("foo"), IsFalse());
+
+            Eval(context, R"(
+- bool foo =: true
+- bool bar =: false
+)");
+            AssertThat(context.VariableCount(), Equals(2));
+            AssertThat(context.VariableExists("foo"), IsTrue());
+            AssertThat(context.VariableExists("bar"), IsTrue());
+
+            auto& foo = context.GetVariable("foo");
+            AssertThat(foo.GetName(), Equals("foo"));
+            AssertThat(foo.GetTypeName(), Equals("bool"));
+
+            auto& fooValue = foo.GetTypedValue();
+            AssertThat(fooValue.GetTypeName(), Equals("bool"));
+            AssertThat(fooValue.GetValue<bool>(), Equals(true));
+            AssertThat(fooValue.GetBoolValue(), Equals(true));
+
+            auto& bar = context.GetVariable("bar");
+            AssertThat(bar.GetName(), Equals("bar"));
+            AssertThat(bar.GetTypeName(), Equals("bool"));
+
+            auto& barValue = bar.GetTypedValue();
+            AssertThat(barValue.GetTypeName(), Equals("bool"));
+            AssertThat(barValue.GetValue<bool>(), Equals(false));
+            AssertThat(barValue.GetBoolValue(), Equals(false));
+        });
+        it("has implicit typing for strings", [&](){
+            auto context = ContextImpl();
+            AssertThat(context.VariableCount(), Equals(0));
+            AssertThat(context.VariableExists("foo"), IsFalse());
+
+            Eval(context, R"(
+- foo =: hello, this is the value of foo!
+)");
+            AssertThat(context.VariableCount(), Equals(1));
+            AssertThat(context.VariableExists("foo"), IsTrue());
+
+            auto& variable = context.GetVariable("foo");
+            AssertThat(variable.GetName(), Equals("foo"));
+            AssertThat(variable.GetTypeName(), Equals("stdlib::string"));
+
+            auto& value = variable.GetTypedValue();
+            AssertThat(value.GetTypeName(), Equals("stdlib::string"));
+            AssertThat(value.GetValue<std::string>(), Equals("hello, this is the value of foo!"));
+            AssertThat(value.GetStringValue(), Equals("hello, this is the value of foo!"));
+        });
+        it("has implicit typing for integers", [&](){
+            auto context = ContextImpl();
+            AssertThat(context.VariableCount(), Equals(0));
+            AssertThat(context.VariableExists("foo"), IsFalse());
+
+            Eval(context, R"(
+- foo =: 69
+)");
+            AssertThat(context.VariableCount(), Equals(1));
+            AssertThat(context.VariableExists("foo"), IsTrue());
+
+            auto& variable = context.GetVariable("foo");
+            AssertThat(variable.GetName(), Equals("foo"));
+            AssertThat(variable.GetTypeName(), Equals("stdlib::int"));
+
+            auto& value = variable.GetTypedValue();
+            AssertThat(value.GetTypeName(), Equals("stdlib::int"));
+            AssertThat(value.GetValue<int64_t>(), Equals(69));
+            AssertThat(value.GetIntValue(), Equals(69));
+        });
+        it("has implicit typing for floats", [&](){
+            auto context = ContextImpl();
+            AssertThat(context.VariableCount(), Equals(0));
+            AssertThat(context.VariableExists("foo"), IsFalse());
+
+            Eval(context, R"(
+- foo =: 69.420
+)");
+            AssertThat(context.VariableCount(), Equals(1));
+            AssertThat(context.VariableExists("foo"), IsTrue());
+
+            auto& variable = context.GetVariable("foo");
+            AssertThat(variable.GetName(), Equals("foo"));
+            AssertThat(variable.GetTypeName(), Equals("stdlib::float"));
+
+            auto& value = variable.GetTypedValue();
+            AssertThat(value.GetTypeName(), Equals("stdlib::float"));
+            AssertThat(value.GetValue<double>(), Equals(69.420));
+            AssertThat(value.GetFloatValue(), Equals(69.420));
+        });
+        it("has implicit typing for booleans", [&](){
+            auto context = ContextImpl();
+            AssertThat(context.VariableCount(), Equals(0));
+            AssertThat(context.VariableExists("foo"), IsFalse());
+
+            Eval(context, R"(
+- foo =: true
+- bar =: false
+)");
+            AssertThat(context.VariableCount(), Equals(2));
+            AssertThat(context.VariableExists("foo"), IsTrue());
+            AssertThat(context.VariableExists("bar"), IsTrue());
+
+            auto& foo = context.GetVariable("foo");
+            AssertThat(foo.GetName(), Equals("foo"));
+            AssertThat(foo.GetTypeName(), Equals("stdlib::bool"));
+
+            auto& fooValue = foo.GetTypedValue();
+            AssertThat(fooValue.GetTypeName(), Equals("stdlib::bool"));
+            AssertThat(fooValue.GetValue<bool>(), Equals(true));
+            AssertThat(fooValue.GetBoolValue(), Equals(true));
+
+            auto& bar = context.GetVariable("bar");
+            AssertThat(bar.GetName(), Equals("bar"));
+            AssertThat(bar.GetTypeName(), Equals("stdlib::bool"));
+
+            auto& barValue = bar.GetTypedValue();
+            AssertThat(barValue.GetTypeName(), Equals("stdlib::bool"));
+            AssertThat(barValue.GetValue<bool>(), Equals(false));
+            AssertThat(barValue.GetBoolValue(), Equals(false));
+        });
     });
 });
